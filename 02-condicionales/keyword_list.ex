0965ff0a8523829c3f4f468dac3f9784971b5960
Camel Negrete Ayala <camelnegrete@gmail.com>
2021-01-27 10:18:40 -0500

    keyword list

---
+++ 02-condicionales/keyword_list.ex
@@ -0,0 +1,9 @@
+list = [a: 1, b: 2]
+#[{}, {}]
+IO.puts()
+
+if (false, [do: "Truhty", else: "Falsy"])
+if (true, [do: "Truhty", else: "Falsy"])
+
+[a: a] = [a: 1, b:2] #Genera error - tamaños direntes
+#[{:a, a}] = [{:a, 1},{:b, 2}]
