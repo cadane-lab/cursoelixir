af46f0f343db65c5ab254037ccc9e71547a72273
Camel Negrete Ayala <camelnegrete@gmail.com>
2021-01-27 16:43:47 -0500

    Ejercicio

--- 03-loops/collatz.ex
+++ 03-loops/collatz.ex
@@ -1,20 +1,19 @@
 defmodule SecuneciaCollatz do
-  def secuencia(n) when is_integer(n) and n == 1 do
+  def secuencia(n) when n == 1 do
     "1 - Fin"
   end
 
-  def secuencia(n) when is_integer(n) and n >= 0 do
+  def secuencia(n) when is_integer(n) and n > 0 do
     IO.puts(n)
-    r = (rem(n,2))
-    if r == 0 do
+    if rem(n,2) == 0 do
       #Es par - se divide por dos
       #divide(n)
-      valor = trunc(n/2)
+      valor = div(n, 2)
       secuencia(valor)
     else
       #Es impar- x3 + 1
       #multiplica(n)
-      valor = trunc((n * 3 ) + 1)
+      valor = n * 3  + 1
       secuencia(valor)
     end
 
